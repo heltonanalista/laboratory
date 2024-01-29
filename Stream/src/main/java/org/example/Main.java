f6c2fa3af689f5f2a78dc2c24458118656a9b8d0
heltonanalista <heltonanalista@gmail.com>
2024-01-29 18:32:17 -0300

    sorted stream

--- Stream/src/main/java/org/example/Main.java
+++ Stream/src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.example.stream.Limit;
-import org.example.stream.Reduce;
-import org.example.stream.Skip;
+import org.example.stream.Sorted;
 
 public class Main {
     public static void main(String[] args) {
 
-        Limit limit=new Limit();
-        limit.limit();
+        Sorted sorted =new Sorted();
+        sorted.sorting();
 
     }
 }
---
+++ Stream/src/main/java/org/example/stream/Sorted.java
@@ -0,0 +1,26 @@
+package org.example.stream;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sorted {
+
+    public void sorting() {
+        List<String> sortingNames = Arrays.asList("ana", "leticia", "pedro", "sofia", "joao", "carlos", "wilson", "bruno", "beatriz","paula");
+        System.out.println("Printing sorted Names in natural order");
+        sortingNames.stream().sorted().forEach(System.out::println);
+
+
+        System.out.println("Printing sorted Names in reversed order");
+
+       sortingNames.stream().sorted(Comparator.comparing(String::toString).reversed()).forEach(System.out::println);
+
+
+
+
+
+
+    }
+
+}
