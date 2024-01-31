9fa4082b74013bf14a522177daf1468555b467c9
heltonanalista <heltonanalista@gmail.com>
2024-01-31 14:16:23 -0300

    add test

--- junit/src/main/java/org/example/Cars.java
+++ junit/src/main/java/org/example/Cars.java
@@ -1,4 +1,7 @@
+package org.example;
+
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Cars {
 
@@ -8,5 +11,76 @@ public class Cars {
     private String color;
     private Double price;
 
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public LocalDate getYear() {
+        return year;
+    }
+
+    public void setYear(LocalDate year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Cars(String branch, String model, LocalDate year, String color, Double price) {
+        this.branch = branch;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "org.example.Cars{" +
+                "branch='" + branch + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars = (Cars) o;
+        return Objects.equals(branch, cars.branch) && Objects.equals(model, cars.model) && Objects.equals(year, cars.year) && Objects.equals(color, cars.color) && Objects.equals(price, cars.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(branch, model, year, color, price);
+    }
 
 }
--- junit/src/test/java/CarsTest.java
+++ junit/src/test/java/CarsTest.java
@@ -1,4 +1,56 @@
+import org.example.Cars;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 public class CarsTest {
+    public Cars cars;
+
+    @BeforeEach
+    public void init() {
+
+       cars = new Cars("ford", "fiesta", LocalDate.of(2012, 01, 10), "silver", 25.300);
+    }
+
+    @Test
+    public void verifyTheBranchOfTheCar() {
+        Assertions.assertEquals("ford", cars.getBranch());
 
 
+    }
+
+    @Test
+    public void verifyTheModelOfTheCar() {
+        Assertions.assertEquals("fiesta", cars.getModel());
+
+
+    }
+    @Test
+    public  void verifyTheYearOfTheCar(){
+        Assertions.assertEquals(LocalDate.of(2012,01,10),cars.getYear());
+
+
+    }
+
+    @Test
+    public void verifyTheColorOfTheCar() {
+        Assertions.assertEquals("silver", cars.getColor());
+
+
+    }
+
+    @Test
+    public void verifyThePriceOfTheCar() {
+        Assertions.assertEquals(25.300, cars.getPrice());
+
+
+    }
+    @Test
+    public void verfyTheCarIsNotNull() {
+        Assertions.assertNotNull(cars);
+
+
+    }
 }
