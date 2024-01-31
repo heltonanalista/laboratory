fc85d87b7b0b30d841fde0a467cf877cf35f5ed6
heltonanalista <heltonanalista@gmail.com>
2024-01-31 14:15:03 -0300

    add cars

---
+++ junit/src/main/java/org/example/Cars.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Cars {
+
+    private  String branch;
+    private String model;
+    private LocalDate year;
+    private String color;
+    private Double price;
+
+
+}
