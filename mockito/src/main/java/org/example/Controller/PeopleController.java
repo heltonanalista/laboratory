15831aa6d2c8eb892792192b7351aae99f873cd6
heltonanalista <heltonanalista@gmail.com>
2024-02-02 12:22:30 -0300

    add people controller

---
+++ mockito/src/main/java/org/example/Controller/PeopleController.java
@@ -0,0 +1,11 @@
+package org.example.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("people")
+public class PeopleController {
+
+
+}
